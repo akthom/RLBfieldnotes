387e0846e85fb4f0aa72d8c7e87d5e008bfcb812
akthom <andrea.thomer@gmail.com>
2016-08-13 15:00:00 -0500

    adding new parser file + txt data

---
+++ rlbParser.py
@@ -0,0 +1,75 @@
+#python 3.4
+#modding from pettigrew parser to start
+
+# from nltk.tag import StanfordNERTagger
+# from nltk.tokenize import word_tokenize
+import nltk, re, pprint, csv
+
+infile='bad.txt'
+
+with open(infile, 'r') as myfile:
+	datas=myfile.read()
+	datas=datas.split('\n\n')
+	
+indDay=[]
+allDays=[]
+
+for i in datas:
+	if i.startswith("Box"):
+		allBoxes.append(indBox)
+#		print(allBoxes)
+		indBox=[]
+		indBox.append(i)
+#		print(indBox)
+		
+	else:
+		indBox.append(i)
+#		print(indBox)
+allBoxes.append(indBox) #necessary to close loop and append last box
+
+
+indLetter=[]
+allLetters=[]
+countyMcCountFace=0
+
+for box in allBoxes:
+	if box ==[]:
+		pass
+	else:
+		letters=box[1::]
+		for i in letters:
+			indLetter=[box[0]]
+			tokens=word_tokenize(i)
+			text=nltk.Text(tokens)
+			coll=text.collocations()
+			V=set(text)
+			long_words = [w for w in V if len(w) > 10]
+			
+			indLetter.append(i)
+			indLetter.append(coll)
+			indLetter.append(long_words)	 #maybe split this up/out later		
+			indLetter.append(len(i))
+		
+			allLetters.append(indLetter)
+		
+			countyMcCountFace=countyMcCountFace+1
+			print(countyMcCountFace)
+			print(indLetter)
+			indLetter=[]
+print ('found ', len(allLetters), ' of Pettigrew\'s papers!')
+
+with open("outputs/pettigrewNERData2.csv","w",newline="") as w:
+	writer=csv.writer(w)
+	writer.writerows(allLetters)
+
+
+TJPtext = '''My Dear Sir, 
+I only received your favour of the 16th late last night, on my return from the Syro-Egyptian, or I would have answered before.  
+I shall be very happy to meet Mr Wright at the Archaeological meeting this evening, the meeting of which society indeed, I have attended on the last occasion.  
+Believe me, 
+My dear sir, 
+Yours very sincerely, 
+W. Francis Ainsworth
+Wednesday Morn
+Dec 17th 1845'''
+	
